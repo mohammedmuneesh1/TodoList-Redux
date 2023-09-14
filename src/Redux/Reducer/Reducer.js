d235bf8ab40ffcff04999b26600ae047e864a51f
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-09-14 11:54:35 +0530

    First Phase Completed

--- src/Redux/Reducer/Reducer.js
+++ src/Redux/Reducer/Reducer.js
@@ -1,19 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const INITIAL_STATE={
+    value:0,
+    theme:true,
+    Ttask:[{
+        id:1,
+        title:"TODO ITEM 1",
+
+    },
+{
+    id:2,
+    title:"TODO ITEM 2"
+}]
+}
+
 export const todoslice=createSlice({
     name:'todo',
-    initialState:{
-        value:0,
-        theme:true
-       },
+    initialState:INITIAL_STATE,
        reducers:{
         ThemeChange: (state) => {
             state.theme = !state.theme;
           },
+          addtask:(state,action)=>{
+            const LID=state.Ttask.reduce((total,value)=> Math.max(total,value.id) ,0);
+            const NewItem={
+                id:LID + 1,
+                title:action.payload,
+                completed:false
+            }
+            state.Ttask.push(NewItem);
+            console.log(state.Ttask)
+          },
+          edittask:(state,action)=>{
+            const {id,title}=action.payload;
+            const UpdatedTaskValue=state.Ttask.find(value=> value.id === id)
+            
+            console.log(UpdatedTaskValue.id);
+            UpdatedTaskValue.title=title;
+          },
+
+          deletetask: (state, action) => {
+            console.log(action.payload)
+            const updatedTasks = state.Ttask.filter((value) => value.id !== action.payload);
+            console.log(updatedTasks);
+            state.Ttask=updatedTasks;
+          },
+    
        },
 
 });
-export const { ThemeChange } = todoslice.actions;
+
+export const { ThemeChange,addtask,edittask,deletetask } = todoslice.actions;
 export default todoslice.reducer
 
 
--- src/Todo/Add.js
+++ src/Todo/Add.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./Todo.css";
+import { useDispatch } from "react-redux";
+import { addtask } from "../Redux/Reducer/Reducer";
 export default function Add() {
+  const dispatch=useDispatch();
+
+
+  const addtaskfn=(e)=>{
+    e.preventDefault(); 
+    const task=e.target.t1.value.trim();
+    if(task===""){
+      alert("Kindly fill out the task  before continuing.")
+      return;
+    }
+    else{
+      dispatch(addtask(task))
+      e.target.reset();
+    }
+  }
+
+
+
   return (
 <>
-<form style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+<form onSubmit={addtaskfn} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
     <div className="add-container">
-      <input type="text" placeholder="Add Item" />
+      <input type="text" name="t1" placeholder="Add Item" />
       <button className="button">Add</button>
     </div>
          </form>
