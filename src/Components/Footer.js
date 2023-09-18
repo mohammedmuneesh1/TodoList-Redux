ebda30e58bcced1e6f415d7753998debca69e838
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-09-18 15:12:50 +0530

    Project Done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   const theme = useSelector(state => state.todo.theme);
 
   return (
-    <div className={`footer ${theme === true ? 'day-footer' : 'dark-footer'}`}>
+    <div className={`footer ${theme? 'day-footer' : 'dark-footer'}`}>
       &copy; {new Date().getFullYear()} Copyright:{' '}
       <b><i className="fab fa-linkedin"></i>:<a className={theme ? 'text-dark':'text-white'}    href='https://www.linkedin.com/in/mohammed-muneesh-e-k-942671274/' target='_blank' rel='noopener noreferrer'>
         Mohammed Muneesh E K
